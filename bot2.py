7b50c3b7a417980f2eeccccddee1e3d491048023
DenysWindows <->
2022-09-01 20:25:04 +0300

    Added main files from Windows (exp)

---
+++ bot2.py
@@ -0,0 +1,59 @@
+import telebot
+import json
+
+import conf
+from telebot import types
+bot = telebot.TeleBot("5380424056:AAFRXRxshbyND6re666VyOk4VFAeWRaWWGQ")
+
+keyboard = types.InlineKeyboardMarkup()
+url_button = types.InlineKeyboardButton(text="Приєднатися!", url='t.me/Memna_hata_bot?start='+str(conf.userClickID)) 
+keyboard.add(url_button)
+
+@bot.message_handler(commands=['newgame'])
+def send_welcome(message):
+	if conf.gameAlreadyStarted == True:
+		bot.reply_to(message, "Гра вже запущена!")
+	else:
+		mesID = bot.send_message(message.chat.id, "Початок реєстрації!\nЩоб зайти натисніть 'Приєднатися!'", reply_markup=keyboard)
+		conf.gameMessageID = mesID.id
+		print(conf.gameMessageID)
+		conf.gameAlreadyStarted = True
+		conf.startPlayerID = message.from_user.id
+		bot.delete_message(message.chat.id, conf.gameMessageID - 1)
+	@bot.message_handler(commands=['crash'])
+	def send_crash(message):
+		if conf.gameMessageID != 0:
+			if message.from_user.id == conf.startPlayerID:
+				if conf.gameAlreadyStarted == True:
+					bot.reply_to(message, "Реєстрація була зупинена!")
+					bot.delete_message(message.chat.id, conf.gameMessageID)
+					conf.gameAlreadyStarted = False
+					conf.gameMessageID = 0
+					conf.startPlayerID = 0
+				else:
+					bot.reply_to(message, "Гра не запущена\nНатисніть /start для запуску гри")
+			else:
+				bot.reply_to(message, "Ви не запускали гру, отже не можете і зупинити")
+		else:
+			bot.reply_to(message, "Гра не була запущена!\nЩоб запустити гру напишіть /start")
+@bot.callback_query_handler(func=lambda call: True)
+def iq_callback(query): #Ця хня по ідеї має хаписувати в Ліст гравців і заповнювати їх але я сам не їбу шо вона робе)))
+	data = query.data
+	js=json.loads(data)
+	print(js)
+	markup = telebot.types.InlineKeyboardMarkup()
+	conf.userClickID = from_user.id
+	conf.userClickName = from_user.username
+	if conf.userClickName in conf.registredPlayers:
+		bot.send_message(message.chat.id, "Ви вже зайшли в гру!")
+	else:
+		bot.edit_message_text(chat_id=message.chat.id, message_id=conf.gameMessageID, text="Початок реєстрації!\nЩоб зайти натисніть 'Приєднатися!'\n\nЗареєстровані:\n" + conf.userClickName + '\n')
+		conf.registredPlayers.append(conf.userClickID)
+	bot.answer_callback_query(query.id, query_text, show_alert=False)
+	bot.edit_message_reply_markup(query.message.chat.id,query.message.message_id,reply_markup=markup)
+
+@bot.message_handler(commands=['help']) #Полезний Хелп знаю)
+def send_welcome(message):
+	bot.reply_to(message, "help")
+
+bot.infinity_polling()
---
+++ conf.py
@@ -0,0 +1,6 @@
+gameAlreadyStarted = False
+startPlayerID = 0 # Чел шо запустив гру
+gameMessageID = 0 # Повідомлення з запущеною грою
+userClickID = 0 # Ід Юзера шо клікнув на кнопку
+userClickName = '' # Ім'я юзера шо клікнув (Для списку гравців)
+registredPlayers = [] # Ідшники гравців
